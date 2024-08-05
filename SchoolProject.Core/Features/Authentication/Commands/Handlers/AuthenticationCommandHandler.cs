6184432c5ec02ecac5ee11061e1df19a58e9483c
useifabdelhady <yoazazy55@gmail.com>
2024-08-05 22:37:54 +0300

    Add JWTToken

---
+++ SchoolProject.Core/Features/Authentication/Commands/Handlers/AuthenticationCommandHandler.cs
@@ -0,0 +1,59 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Localization;
+using SchoolProject.Core.Bases;
+using SchoolProject.Core.Features.Authentication.Commands.Models;
+using SchoolProject.Core.Resources;
+using SchoolProject.Data.Entities.Identity;
+using SchoolProject.Service.Abstracts;
+
+namespace SchoolProject.Core.Features.Authentication.Commands.Handlers
+{
+    public class AuthenticationCommandHandler : ResponseHandler,
+                IRequestHandler<SignInCommand, Response<string>>
+    {
+
+        #region Fields
+        private readonly IStringLocalizer<SharedResources> _stringLocalizer;
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly IAuthenticationService _authenticationService;
+
+        #endregion
+        #region Constructors
+        public AuthenticationCommandHandler(IStringLocalizer<SharedResources> stringLocalizer,
+                                                                         UserManager<User> userManager,
+                                                                         SignInManager<User> signInManager,
+                                                                          IAuthenticationService authenticationService) : base(stringLocalizer)
+        {
+            _stringLocalizer = stringLocalizer;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _authenticationService = authenticationService;
+        }
+
+
+        #endregion
+        #region Handel Functions
+        public async Task<Response<string>> Handle(SignInCommand request, CancellationToken cancellationToken)
+        {
+            //Check if user is exist or not
+            var user = await _userManager.FindByNameAsync(request.UserName);
+            //Return The UserName Not Found
+            if (user == null) return BadRequest<string>(_stringLocalizer[SharedResourcesKeys.UserNameIsNotExist]);
+            //try To Sign in 
+            var signInResult = await _signInManager.CheckPasswordSignInAsync(user, request.Password, false);
+            //if Failed Return Passord is wrong
+            if (!signInResult.Succeeded) return BadRequest<string>(_stringLocalizer[SharedResourcesKeys.PasswordNotCorrect]);
+            //confirm email
+            /* if (!user.EmailConfirmed)
+                 return BadRequest<JwtAuthResult>(_stringLocalizer[SharedResourcesKeys.EmailNotConfirmed]);*/
+            //Generate Token
+            var result = await _authenticationService.GetJWTToken(user);
+            //return Token 
+            return Success(result);
+        }
+        #endregion
+
+    }
+}
